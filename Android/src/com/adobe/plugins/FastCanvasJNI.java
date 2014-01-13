ba47598b741dd9ee2a54bfcff509261ffe71d635
Ali Shakiba <ali@shakiba.me>
2014-01-13 10:14:53 +0330

    Java rewrite, Cordova 3

--- Android/src/com/adobe/plugins/FastCanvasJNI.java
+++ Android/src/com/adobe/plugins/FastCanvasJNI.java
@@ -13,24 +13,42 @@
  limitations under the License.
  */
 
-
 package com.adobe.plugins;
 
 public class FastCanvasJNI {
-	// Native methods
-	public static native void setBackgroundColor(int red, int green, int blue);
-	public static native void setOrtho(int width, int height);
-	public static native void addTexture(int id, int glID, int width, int height); // id's must be from 0 to numTextures-1
-	public static native boolean addPngTexture(Object mgr, String path, int id, FastCanvasTextureDimension dim); // id's must be from 0 to numTextures-1
-	public static native void removeTexture(int id); // id must have been passed to addTexture in the past
+    // Native methods
+
+    public static native void setBackgroundColor(int red, int green, int blue);
+
+    public static native void setOrtho(int width, int height);
+
+    // id's must be from 0 to numTextures-1
+    public static native void addTexture(int id, int glID, int width, int height);
+
+    // id's must be from 0 to numTextures-1
+    public static native boolean addPngTexture(Object mgr, String path, int id,
+            FastCanvasTextureDimension dim);
+
+    // id must have been passed to addTexture in the past
+    public static native void removeTexture(int id);
+
     public static native void render(String renderCommands);
-	public static native void surfaceChanged( int width, int height );
-	public static native void captureGLLayer(String callbackID, int x, int y, int width, int height, String fileName); //captures the current contents of the GL layer and writes to a temporary file
-	public static native void contextLost(); // Deletes native memory associated with lost GL context
-	public static native void release(); // Deletes native canvas
-	
-	// Load library
-	static {
-		System.loadLibrary("FastCanvasJNI");
-	}
+
+    public static native void surfaceChanged(int width, int height);
+
+    // captures the current contents of the GL layer and writes to a temporary
+    // file
+    public static native void captureGLLayer(String callbackID, int x, int y,
+            int width, int height, String fileName);
+
+    // Deletes native memory associated with lost GL context
+    public static native void contextLost();
+
+    // Deletes native canvas
+    public static native void release();
+
+    // Load library
+    static {
+        System.loadLibrary("FastCanvasJNI");
+    }
 }
--- Android/src/com/adobe/plugins/FastCanvasView.java
+++ Android/src/com/adobe/plugins/FastCanvasView.java
@@ -16,94 +16,360 @@
  */
 package com.adobe.plugins;
 
-import android.opengl.GLSurfaceView;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
+import java.io.File;
+import java.io.IOException;
+import java.nio.IntBuffer;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES10;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
+import android.os.Environment;
+import android.util.Log;
+import android.view.SurfaceHolder;
 
 public class FastCanvasView extends GLSurfaceView {
 
-	public FastCanvasView(Context context) {
-		super(context);
-		this.setEGLConfigChooser( false );// turn off the depth buffer
-		mRenderer = new FastCanvasRenderer(this);
-		this.setRenderer(mRenderer);
-		this.setRenderMode(RENDERMODE_CONTINUOUSLY);
-		
-		this.setFocusableInTouchMode(true);
-		this.requestFocus();
-	}
+    private FastCanvasRenderer renderer;
+    private LinkedList<TextureRequest> loadQueue = new LinkedList<TextureRequest>();
+    private Map<Integer, Texture> textures = new HashMap<Integer, Texture>();
+    private LinkedList<CallbackContext> frameRequests = new LinkedList<CallbackContext>();
+    private String renderCommand;
 
-	@Override
-	public boolean onKeyDown (int keyCode, KeyEvent event) {
-		synchronized (this) {
-			if (FastCanvas.dispatchKeyDown(keyCode, event)) {
-				return true;
-			} else {
-				return super.onKeyDown(keyCode, event);
-			}
-		}
-	}
+    public FastCanvasView(Context context, FastCanvas plugin) {
+        super(context);
+        this.setEGLConfigChooser(false);// turn off the depth buffer
+        this.renderer = new FastCanvasRenderer();
+        this.setRenderer(renderer);
+        this.setRenderMode(RENDERMODE_CONTINUOUSLY);
 
-	@Override
-	public boolean onKeyUp (int keyCode, KeyEvent event) {
-		synchronized (this) {
-			if (FastCanvas.dispatchKeyUp(keyCode, event)) {
-				return true;
-			} else {
-				return super.onKeyDown(keyCode, event);
-			}
-		}
-	}
+        this.setFocusableInTouchMode(true);
+        this.requestFocus();
+    }
 
-	@Override
-	public boolean onTouchEvent (MotionEvent event) {
-		synchronized (this) {
-			if (!FastCanvas.dispatchTouchEvent(event)) {
-				// tear down.
-				mRenderer = null;				
-			}
-		}
-		return true;
-	}
-	
-	public void surfaceCreated(SurfaceHolder holder)
-	{
-        Log.i("CANVAS", "CanvasView surfaceCreated");
-	    super.surfaceCreated(holder);
-	    if (isPaused) {
-	    	isPaused = false;
-	    	mRenderer.reloadTextures();
-	    	super.onResume();
-	    }
-			}
-	
-	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h)
-	{
-        Log.i("CANVAS", "CanvasView surfaceChanged");
-	    super.surfaceChanged(holder, format, w, h);
-		}
-	
-	public void surfaceDestroyed(SurfaceHolder holder)
-	{
-		// Context is gone, no GL calls after this
-        Log.i("CANVAS", "CanvasView surfaceDestroyed");
-		isPaused = true;
-	    super.surfaceDestroyed(holder);
-		super.onPause();
-		FastCanvasJNI.contextLost();
-		mRenderer.onSurfaceDestroyed();
-		
-		// Avoid crashing on open-home-open
-		System.runFinalization();
-		android.os.Process.killProcess(android.os.Process.myPid());
-	}
-	
-	public void execScripts(Object obj) {
-	}
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.i(TAG, "surfaceCreated");
+        super.surfaceCreated(holder);
+    }
 
-	private FastCanvasRenderer mRenderer;
-	public boolean isPaused = false;
+    @Override
+    public void onPause() {
+        Log.i(TAG, "onPause");
+        FastCanvasJNI.contextLost();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        Log.i(TAG, "onResume");
+
+        for (Texture t : textures.values()) {
+            TextureRequest m = new TextureRequest(t.url, t.id, null);
+            Log.i(TAG, "queueing reload textures " + m.texture);
+            loadQueue.add(m);
+        }
+
+        super.onResume();
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        Log.i(TAG, "surfaceChanged");
+        super.surfaceChanged(holder, format, w, h);
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Context is gone, no GL calls after this
+        Log.i(TAG, "surfaceDestroyed");
+        super.surfaceDestroyed(holder);
+        FastCanvasJNI.release();
+    }
+
+    public boolean execute(String action, JSONArray args,
+            final CallbackContext callbackContext) throws JSONException {
+        // Log.i(TAG, "execute: " + action);
+
+        try {
+
+            if (action.equals("render")) {
+                renderCommand = args.getString(0);
+                return true;
+
+            } else if (action.equals("frameRequest")) {
+                // Log.i(TAG, "frameRequest");
+
+                frameRequests.offer(callbackContext);
+                PluginResult result = new PluginResult(
+                        PluginResult.Status.NO_RESULT);
+                result.setKeepCallback(true);
+                callbackContext.sendPluginResult(result);
+
+                CallbackContext frameRequest;
+                int size = frameRequests.size();
+                while (size-- > 0
+                        && (frameRequest = frameRequests.poll()) != null) {
+                    // Log.i(TAG, "frameResponse");
+                    PluginResult resultx = new PluginResult(
+                            PluginResult.Status.OK);
+                    resultx.setKeepCallback(false);
+                    frameRequest.sendPluginResult(resultx);
+                }
+
+            } else if (action.equals("setBackgroundColor")) {
+                String color = args.getString(0);
+                Log.i(TAG, "setBackground: " + color);
+                try {
+                    int red = Integer.valueOf(color.substring(0, 2), 16);
+                    int green = Integer.valueOf(color.substring(2, 4), 16);
+                    int blue = Integer.valueOf(color.substring(4, 6), 16);
+                    FastCanvasJNI.setBackgroundColor(red, green, blue);
+                } catch (Exception e) {
+                    Log.e(TAG, "Invalid background color: " + color, e);
+                }
+
+                return true;
+
+            } else if (action.equals("loadTexture")) {
+                TextureRequest m = new TextureRequest(args.getString(0),
+                        args.getInt(1), callbackContext);
+                Log.i(TAG, "queueing load texture " + m.texture);
+                loadQueue.add(m);
+                return true;
+
+            } else if (action.equals("unloadTexture")) {
+                Log.i(TAG, "unload texture");
+                int id = args.getInt(0);
+                textures.remove(id);
+                FastCanvasJNI.removeTexture(id);
+                checkError();
+
+                return true;
+
+            } else if (action.equals("setOrtho")) {
+                int width = args.getInt(0);
+                int height = args.getInt(1);
+
+                Log.i(TAG, "setOrtho: " + width + ", " + height);
+                FastCanvasJNI.setOrtho(width, height);
+
+                return true;
+
+            } else if (action.equals("capture")) {
+                Log.i(TAG, "capture");
+
+                // set the root path to /mnt/sdcard/
+                String file = Environment.getExternalStorageDirectory()
+                        + args.getString(4);
+                File dir = new File(file).getParentFile();
+                if (!dir.isDirectory() && !dir.mkdirs()) {
+                    callbackContext.sendPluginResult(new PluginResult(
+                            PluginResult.Status.ERROR,
+                            "Could not create directory"));
+                    return true;
+                }
+
+                int x = args.optInt(0, 0);
+                int y = args.optInt(1, 0);
+                int width = args.optInt(2, -1);
+                int height = args.optInt(3, -1);
+
+                FastCanvasJNI.captureGLLayer(callbackContext.getCallbackId(),
+                        x, y, width, height, file);
+
+                return true;
+
+            } else if (action.equals("isAvailable")) {
+                callbackContext.sendPluginResult(new PluginResult(
+                        PluginResult.Status.OK, true));
+                return true;
+
+            } else {
+                Log.i(TAG, "invalid execute action: " + action);
+            }
+
+        } catch (Exception e) {
+            Log.e(TAG, "error executing action: " + action + "(" + args + ")",
+                    e);
+        }
+
+        return false;
+    }
+
+    public class FastCanvasRenderer implements GLSurfaceView.Renderer {
+
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+            Log.i(TAG, "onSurfaceCreated: " + gl + ", " + config);
+
+            TextureRequest m;
+            while ((m = loadQueue.poll()) != null) {
+                loadTexture(m.callbackContext, m.texture);
+            }
+
+            IntBuffer ib = IntBuffer.allocate(100);
+            ib.position(0);
+            GLES10.glGetIntegerv(GLES10.GL_RED_BITS, ib);
+            int red = ib.get(0);
+            GLES10.glGetIntegerv(GLES10.GL_GREEN_BITS, ib);
+            int green = ib.get(0);
+            GLES10.glGetIntegerv(GLES10.GL_BLUE_BITS, ib);
+            int blue = ib.get(0);
+            GLES10.glGetIntegerv(GLES10.GL_STENCIL_BITS, ib);
+            int stencil = ib.get(0);
+            GLES10.glGetIntegerv(GLES10.GL_DEPTH_BITS, ib);
+            int depth = ib.get(0);
+
+            Log.i(TAG, "onSurfaceCreated R: " + red + " G: " + green + " B: "
+                    + blue + " DEPETH: " + depth + " STENCIL: " + stencil);
+
+        }
+
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            Log.i(TAG, "onSurfaceChanged");
+            FastCanvasJNI.surfaceChanged(width, height);
+        }
+
+        public void onDrawFrame(GL10 gl) {
+            FastCanvasJNI.render(renderCommand);
+            checkError();
+        }
+
+        private static final String TAG = "FastCanvasRenderer";
+    }
+
+    private void checkError() {
+        int error = GLES10.glGetError();
+        if (error != GLES10.GL_NO_ERROR) {
+            Log.i(TAG, "glError=" + error);
+        }
+        assert error == GLES10.GL_NO_ERROR;
+    }
+
+    private void loadTexture(CallbackContext callback, Texture texture) {
+        Log.i(TAG, "load texture: " + texture);
+
+        if (textures.containsKey(texture.id)) {
+            FastCanvasJNI.removeTexture(texture.id);
+            checkError();
+        }
+
+        try {
+            FastCanvasTextureDimension dim = loadTexture(texture);
+
+            if (callback != null) {
+                textures.put(texture.id, texture);
+                JSONArray args = new JSONArray();
+                args.put(dim.width);
+                args.put(dim.height);
+                callback.success(args);
+            }
+
+        } catch (Exception e) {
+            Log.i(TAG, "load texture error: ", e);
+            if (callback != null) {
+                callback.error(e.getMessage());
+            }
+        }
+    }
+
+    private FastCanvasTextureDimension loadTexture(Texture texture)
+            throws IOException {
+
+        FastCanvasTextureDimension dim = new FastCanvasTextureDimension();
+
+        String path = "www/" + texture.url;
+
+        AssetManager assets = getContext().getAssets();
+
+        // PNG files with premultiplied
+        // alpha and GLUtils don't get along
+        // http://stackoverflow.com/questions/3921685
+        if (path.toLowerCase(Locale.US).endsWith(".png")) {
+            if (FastCanvasJNI.addPngTexture(assets, path, texture.id, dim)) {
+                return dim;
+            }
+            Log.i(TAG, "native PNG load filaed, falling back to GLUtils");
+        }
+
+        Bitmap bmp = BitmapFactory.decodeStream(assets.open(path));
+
+        int[] glID = new int[1];
+        GLES10.glGenTextures(1, glID, 0);
+        GLES10.glBindTexture(GLES10.GL_TEXTURE_2D, glID[0]);
+        GLES10.glTexParameterf(GLES10.GL_TEXTURE_2D,
+                GLES10.GL_TEXTURE_MIN_FILTER, GLES10.GL_LINEAR);
+        GLES10.glTexParameterf(GLES10.GL_TEXTURE_2D,
+                GLES10.GL_TEXTURE_MAG_FILTER, GLES10.GL_LINEAR);
+
+        int width = bmp.getWidth(), height = bmp.getHeight();
+        int p2width = 2, p2height = 2;
+        while (p2width < width)
+            p2width *= 2;
+        while (p2height < height)
+            p2height *= 2;
+
+        if (width == p2width && height == p2height) {
+            GLUtils.texImage2D(GLES10.GL_TEXTURE_2D, 0, bmp, 0);
+        } else {
+            Log.i(TAG, "load texture scaling texture " + texture
+                    + " to power of 2");
+            GLES10.glTexImage2D(GLES10.GL_TEXTURE_2D, 0, GLES10.GL_RGBA,
+                    p2width, p2height, 0, GLES10.GL_RGBA,
+                    GLES10.GL_UNSIGNED_BYTE, null);
+            GLUtils.texSubImage2D(GLES10.GL_TEXTURE_2D, 0, 0, 0, bmp);
+            width = p2width;
+            height = p2height;
+        }
+
+        checkError();
+
+        FastCanvasJNI.addTexture(texture.id, glID[0], width, height);
+        Log.i(TAG, "load texture done: " + texture);
+
+        dim.width = bmp.getWidth();
+        dim.height = bmp.getHeight();
+
+        return dim;
+    }
+
+    class Texture {
+        public final String url;
+        public final int id;
+
+        public Texture(String url, int id) {
+            this.url = url;
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return url + "#" + id + "@" + hashCode();
+        }
+    }
+
+    class TextureRequest {
+        public final Texture texture;
+        public final CallbackContext callbackContext;
+
+        public TextureRequest(String url, int id,
+                CallbackContext callbackContext) {
+            this.texture = new Texture(url, id);
+            this.callbackContext = callbackContext;
+        }
+    }
+
+    private static final String TAG = "FastCanvasView";
 }
